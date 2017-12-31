7059451086dbe8de32ab53ef5cad5a63dc4af538
Aditya Dhananjay Wagholikar <waditya02@gmail.com>
2017-12-31 13:01:57 -0600

    Update 02_Intro_to_Tutorial_Challenges.py

--- 02_Intro_to_Tutorial_Challenges.py
+++ 02_Intro_to_Tutorial_Challenges.py
@@ -1,1 +1,18 @@
+#!/bin/python3
+
+import sys
+
+def introTutorial(V, arr):
+    for i in range(0, len(arr), 1):
+        if arr[i] == V:
+            index = i
+            break
+    return(index)
+
+if __name__ == "__main__":
+    V = int(input().strip())
+    n = int(input().strip())
+    arr = list(map(int, input().strip().split(' ')))
+    result = introTutorial(V, arr)
+    print(result)
 
